5dae5b241f8cf471a1fe7621608bbd76105e273e
Ron Jaegers <ron.jaegers@gmail.com>
2015-04-15 18:06:18 +0200

    Implemented RAII interrupt disable/enable class

--- source/Common/DisableInterruptContext.h
+++ source/Common/DisableInterruptContext.h
@@ -1,10 +1,26 @@
 #pragma once
 
+#ifndef BUILD_FOR_EMULATOR
+	#include "WProgram.h"
+#endif
+
 namespace Common
 {
     class DisableInterruptContext
     {
-        DisableInterruptContext() {}
-        ~DisableInterruptContext() {}
+	public:
+		DisableInterruptContext()
+		{
+#ifndef BUILD_FOR_EMULATOR
+			cli();
+#endif
+		}
+
+		~DisableInterruptContext()
+		{
+#ifndef BUILD_FOR_EMULATOR
+			sei();
+#endif
+		}
     };
 }
