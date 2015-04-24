de0d26e2e22ff9e4bc3d0bec2fbda977bfe096d6
Ron Jaegers <ron.jaegers@gmail.com>
2015-04-24 18:44:29 +0200

    Assigned pin number to VSS pin, fixes build

--- source/HardwareLayer/Teensy/TeensyHardware.cpp
+++ source/HardwareLayer/Teensy/TeensyHardware.cpp
@@ -4,6 +4,7 @@ HardwareLayer::TeensyHardware::TeensyHardware()
 	: m_XPin(A2)
 	, m_YPin(A3)
 	, m_ZPin(A4)
+	, m_VSSPin(23)
 	, m_AccelerometerDriver(m_XPin, m_YPin, m_ZPin, m_Timer)
 {
 	m_Platform.Init();
