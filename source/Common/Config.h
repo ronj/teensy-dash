90c6d59a6a0c4d97b53e7af0df5502ae4f4f21be
Ron Jaegers <ron.jaegers@gmail.com>
2015-05-04 19:43:38 +0200

    Fix compiler feature detection; clang seems to also declare __GNUC__ so it has to be checked first.

--- source/Common/Config.h
+++ source/Common/Config.h
@@ -8,51 +8,51 @@
   #define CFG_HAS_DECLSPEC
 #endif
 
-// C++11 support for different compilers
-#if defined(__GNUC__)
-#  define GCC_VERSION (__GNUC__ * 10000 \
-    + __GNUC_MINOR__ * 100 \
-    + __GNUC_PATCHLEVEL__)
-#  if GCC_VERSION >= 40300
-#    define CFG_HAS_VARIADIC_TEMPLATES 1
-#    define CFG_HAS_RVALUE_REFERENCES 1
-#  endif
-#  if GCC_VERSION >= 40400
-#    define CFG_HAS_DELETED_FUNCTIONS 1
-#  endif
-#  if GCC_VERSION >= 40500
-#    define CFG_HAS_EXPLICIT_CONVERSION_OPERATORS 1
-#    define CFG_HAS_STRING_LITERALS 1
-#  endif
-#  if GCC_VERSION >= 40600
-#    define CFG_HAS_RANGE_FOR 1
-#    define CFG_HAS_CONSTEXPR 1
-#  endif
-#  if GCC_VERSION >= 40700
-#    define CFG_HAS_ALIAS_TEMPLATES 1
-#  endif
-#elif defined(__clang__)
-#  define CFG_HAS_VARIADIC_TEMPLATES __has_feature(cxx_variadic_templates)
-#  define CFG_HAS_RVALUE_REFERENCES  __has_feature(cxx_rvalue_references)
-#  define CFG_HAS_DELETED_FUNCTIONS  __has_feature(cxx_deleted_functions)
-#  define CFG_HAS_EXPLICIT_CONVERSION_OPERATORS __has_feature(cxx_explicit_conversions)
-#  define CFG_HAS_RANGE_FOR          __has_feature(cxx_range_for)
-#  define CFG_HAS_ALIAS_TEMPLATES    __has_feature(cxx_alias_templates)
-#  define CFG_HAS_CONSTEXPR          __has_feature(cxx_constexpr)
-#  define CFG_HAS_STRING_LITERALS    __has_feature(cxx_raw_string_literals)
+// C++11 feature support for different compilers
+#if defined(__clang__) || defined(__llvm__)
+  #define CFG_HAS_VARIADIC_TEMPLATES            __has_feature(cxx_variadic_templates)
+  #define CFG_HAS_RVALUE_REFERENCES             __has_feature(cxx_rvalue_references)
+  #define CFG_HAS_DELETED_FUNCTIONS             __has_feature(cxx_deleted_functions)
+  #define CFG_HAS_EXPLICIT_CONVERSION_OPERATORS __has_feature(cxx_explicit_conversions)
+  #define CFG_HAS_RANGE_FOR                     __has_feature(cxx_range_for)
+  #define CFG_HAS_ALIAS_TEMPLATES               __has_feature(cxx_alias_templates)
+  #define CFG_HAS_CONSTEXPR                     __has_feature(cxx_constexpr)
+  #define CFG_HAS_STRING_LITERALS               __has_feature(cxx_raw_string_literals)
+#elif defined(__GNUC__)
+  #define GCC_VERSION(__GNUC__ * 10000 + \
+                      __GNUC_MINOR__ * 100 + \
+                      __GNUC_PATCHLEVEL__)
+  #if GCC_VERSION >= 40300
+    #define CFG_HAS_VARIADIC_TEMPLATES 1
+    #define CFG_HAS_RVALUE_REFERENCES 1
+  #endif
+  #if GCC_VERSION >= 40400
+    #define CFG_HAS_DELETED_FUNCTIONS 1
+  #endif
+  #if GCC_VERSION >= 40500
+    #define CFG_HAS_EXPLICIT_CONVERSION_OPERATORS 1
+    #define CFG_HAS_STRING_LITERALS 1
+  #endif
+  #if GCC_VERSION >= 40600
+    #define CFG_HAS_RANGE_FOR 1
+    #define CFG_HAS_CONSTEXPR 1
+  #endif
+  #if GCC_VERSION >= 40700
+    #define CFG_HAS_ALIAS_TEMPLATES 1
+  #endif
 #elif defined(_MSC_VER)
 // No variadics, no deleted functions, no fun :(
-#  if _MSC_VER > 1500
-#    define CFG_HAS_RVALUE_REFERENCES 1
-#  endif
-#  if _MSC_VER > 1600
-#    define CFG_HAS_RANGE_FOR 1
-#    if MSC_NOV_2012_CTP || _MSC_VER > 1700
-#      define CFG_HAS_VARIADIC_TEMPLATES 1
-#      define CFG_HAS_EXPLICIT_CONVERSION_OPERATORS 1
-#      define CFG_HAS_DELEGATING_CONSTRUCTORS 1
-#    endif
-#  endif
+  #if _MSC_VER > 1500
+    #define CFG_HAS_RVALUE_REFERENCES 1
+  #endif
+  #if _MSC_VER > 1600
+    #define CFG_HAS_RANGE_FOR 1
+    #if MSC_NOV_2012_CTP || _MSC_VER > 1700
+      #define CFG_HAS_VARIADIC_TEMPLATES 1
+      #define CFG_HAS_EXPLICIT_CONVERSION_OPERATORS 1
+      #define CFG_HAS_DELEGATING_CONSTRUCTORS 1
+    #endif
+  #endif
 #endif
 
 #if !defined(CFG_HAS_VARIADIC_TEMPLATES)
