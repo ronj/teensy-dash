e02a5cc0b91a3cbe91e30bc435893c62abb6c47f
Ron Jaegers <ron.jaegers@gmail.com>
2015-07-02 20:55:08 +0200

    Change formatting of VoltageModel.

--- source/ApplicationLayer/Models/VoltageModel.cpp
+++ source/ApplicationLayer/Models/VoltageModel.cpp
@@ -18,11 +18,23 @@ int32_t ApplicationLayer::Models::VoltageModel::GetRawValue() const
 const char* ApplicationLayer::Models::VoltageModel::GetFormattedValue() const
 {
 	static char formatted[5] = { 0 };
-	int32_t rawValue = GetRawValue();
 
-	itoa2(rawValue / 100, formatted, 10);
-	formatted[1] = '.';
-	itoa2(rawValue % 100, formatted + 2, 10);
+	int32_t rawValue = GetRawValue();
+	int16_t integerPart = rawValue / 100;
+	int16_t fractionalPart = rawValue % 100;
+
+	if (integerPart > 10)
+	{
+		itoa2(integerPart, formatted, 10);
+		formatted[2] = '.';
+		itoa2(fractionalPart, formatted + 3, 10);
+	}
+	else
+	{
+		itoa2(integerPart, formatted, 10);
+		formatted[1] = '.';
+		itoa2(fractionalPart, formatted + 2, 10);
+	}
 
 	return formatted;
 }
