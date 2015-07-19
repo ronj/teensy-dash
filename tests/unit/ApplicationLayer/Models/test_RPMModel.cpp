6d75e46806dfaff7066a709993ad6bbca6185616
Ron Jaegers <ron.jaegers@gmail.com>
2015-07-19 22:46:04 +0200

    Migrate test to frequency counting as well.

--- tests/unit/ApplicationLayer/Models/test_RPMModel.cpp
+++ tests/unit/ApplicationLayer/Models/test_RPMModel.cpp
@@ -1,19 +1,19 @@
 #include "yaffut.h"
 #include "hippomocks.h"
 
-#include "PeripheralLayer/PulseCounter.h"
+#include "PeripheralLayer/FrequencyCounter.h"
 
 #include "ApplicationLayer/Models/RPMModel.h"
 
 struct RPMModelTest
 {
 	MockRepository mocks;
-	PeripheralLayer::PulseCounter* pulseCounter = mocks.Mock<PeripheralLayer::PulseCounter>();
+	PeripheralLayer::FrequencyCounter* frequencyCounter = mocks.Mock<PeripheralLayer::FrequencyCounter>();
 };
 
 TEST(RPMModelTest, should_have_correct_initial_value)
 {
-	ApplicationLayer::Models::RPMModel rpm(*pulseCounter);
+	ApplicationLayer::Models::RPMModel rpm(*frequencyCounter);
 
 	EXPECT_EQ(0, rpm.GetRawValue());
 	EXPECT_EQ("0", rpm.GetFormattedValue());
