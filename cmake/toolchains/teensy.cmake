edaff8351ca143f634011bc9fac96a520e2423c7
Ron Jaegers <ron.jaegers@gmail.com>
2015-11-21 14:18:21 +0100

    Begin of refactoring of toolchain file. Also support Windows and Linux.

--- cmake/toolchains/teensy.cmake
+++ cmake/toolchains/teensy.cmake
@@ -1,7 +1,64 @@
 # This toolchain file is based on https://github.com/apmorton/teensy-template/blob/master/Makefile
+# and https://github.com/queezythegreat/arduino-cmake
+# and https://github.com/sudar/Arduino-Makefile
 # and on the Teensy Makefile.
 
-include(CMakeForceCompiler)
+if(UNIX)
+    include(Platform/UnixPaths)
+    if(APPLE)
+        list(APPEND CMAKE_SYSTEM_PREFIX_PATH ~/Applications
+                                             /Applications
+                                             /Developer/Applications
+                                             /sw         # Fink
+                                             /opt/local) # MacPorts
+    endif()
+elseif(WIN32)
+    include(Platform/WindowsPaths)
+endif()
+
+if(NOT ARDUINO_SDK_PATH)
+    set(ARDUINO_PATHS)
+
+    foreach(DETECT_VERSION_MAJOR 1)
+        foreach(DETECT_VERSION_MINOR RANGE 6 0)
+            list(APPEND ARDUINO_PATHS arduino-${DETECT_VERSION_MAJOR}.${DETECT_VERSION_MINOR})
+            foreach(DETECT_VERSION_PATCH  RANGE 5 0)
+                list(APPEND ARDUINO_PATHS arduino-${DETECT_VERSION_MAJOR}.${DETECT_VERSION_MINOR}.${DETECT_VERSION_PATCH})
+            endforeach()
+        endforeach()
+    endforeach()
+
+    foreach(VERSION RANGE 23 19)
+        list(APPEND ARDUINO_PATHS arduino-00${VERSION})
+    endforeach()
+
+    if(UNIX)
+        file(GLOB SDK_PATH_HINTS /usr/share/arduino*
+                                 /opt/local/arduino*
+                                 /opt/arduino*
+                                 /usr/local/share/arduino*)
+    elseif(WIN32)
+        set(SDK_PATH_HINTS "C:\\Program Files\\Arduino"
+                           "C:\\Program Files (x86)\\Arduino")
+    endif()
+    list(SORT SDK_PATH_HINTS)
+    list(REVERSE SDK_PATH_HINTS)
+endif()
+
+find_path(ARDUINO_SDK_PATH
+          NAMES lib/version.txt
+          PATH_SUFFIXES share/arduino
+                        Arduino.app/Contents/Resources/Java/
+                        ${ARDUINO_PATHS}
+          HINTS ${SDK_PATH_HINTS}
+          DOC "Arduino SDK path.")
+
+if(ARDUINO_SDK_PATH)
+    list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${ARDUINO_SDK_PATH}/hardware/tools/arm)
+else()
+    message(FATAL_ERROR "Could not find Arduino SDK (set ARDUINO_SDK_PATH)!")
+endif()
+
 
 
 # User configurable variables
@@ -9,7 +66,7 @@ include(CMakeForceCompiler)
 #
 set(TEENSY_VERSION 31 CACHE STRING "Set to the Teensy version corresponding to your board (30 or 31 allowed)" FORCE)
 set(CPU_CORE_SPEED 96000000 CACHE STRING "Set to 24000000, 48000000, 72000000 or 96000000 to set CPU core speed" FORCE)
-set(ARDUINOPATH "/Applications/Arduino.app/Contents/Resources/Java/" CACHE STRING "Path to Arduino installation" FORCE)
+set(ARDUINOPATH "/Users/ron/Downloads/Arduino.app/Contents/Resources/Java/" CACHE STRING "Path to Arduino installation" FORCE)
 
 
 # Derived variables
@@ -28,12 +85,8 @@ set(LIBRARYPATH "${ARDUINOPATH}hardware/teensy/avr/libraries/")
 # this one is important
 set(CMAKE_SYSTEM_NAME Generic)
 
-# specify the cross compiler
-cmake_force_c_compiler(${COMPILERPATH}arm-none-eabi-gcc GNU)
-cmake_force_cxx_compiler(${COMPILERPATH}arm-none-eabi-g++ GNU)
-
 # where is the target environment 
-set(CMAKE_FIND_ROOT_PATH ${COMPILERPATH})
+#set(CMAKE_FIND_ROOT_PATH ${COMPILERPATH})
 
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -68,18 +121,18 @@ macro(get_directorylist RESULT DIR)
 endmacro()
 
 get_directorylist(ARDUINO_LIBRARY_INCLUDES ${LIBRARYPATH})
-set(ARDUINO_INCLUDES ${ARDUINO_LIBRARY_INCLUDES};${COREPATH};${LIBRARYPATH}Ethernet/utility;${LIBRARYPATH}SD/utility;${LIBRARYPATH}Wire/utility;${LIBRARYPATH}Snooze/utility)
+set(ARDUINO_INCLUDES ${ARDUINO_LIBRARY_INCLUDES};${COREPATH};${LIBRARYPATH}Adafruit_nRF8001/utility;${LIBRARYPATH}Audio/utility;${LIBRARYPATH}Ethernet/utility;${LIBRARYPATH}Keypad/utility;${LIBRARYPATH}SD/utility;${LIBRARYPATH}Snooze/utility;${LIBRARYPATH}VirtualWire/utility;${LIBRARYPATH}Wire/utility)
 
 
 # Teensy 3.0 and 3.1 differentiation
 #
 
 if (TEENSY_VERSION EQUAL 30)
-	set(CPU_DEFINE __MK20DX128__)
-	set(LINKER_FILE ${COREPATH}mk20dx128.ld)
+    set(CPU_DEFINE __MK20DX128__)
+    set(LINKER_FILE ${COREPATH}mk20dx128.ld)
 else()
-	set(CPU_DEFINE __MK20DX256__)
-	set(LINKER_FILE ${COREPATH}mk20dx256.ld)
+    set(CPU_DEFINE __MK20DX256__)
+    set(LINKER_FILE ${COREPATH}mk20dx256.ld)
 endif()
 
 set(TEENSY_DEFINITIONS USB_SERIAL
@@ -93,30 +146,30 @@ set(TEENSY_DEFINITIONS USB_SERIAL
 # Macros to wrap add_[executable|library] for seamless Teensy integration
 #
 function(teensy_add_executable TARGET)
-	set(ELFTARGET ${TARGET}.elf)
+    set(ELFTARGET ${TARGET}.elf)
 
     add_executable(${TARGET} ${ARGN} ${TEENSY_SOURCES})
 
     set_target_properties(${TARGET} PROPERTIES COMPILE_FLAGS "-Wall -g -Os -mcpu=${CPU} -mthumb -nostdlib -MMD -felide-constructors -fno-exceptions -fno-rtti -std=gnu++11")
-	set_target_properties(${TARGET} PROPERTIES COMPILE_DEFINITIONS "${TEENSY_DEFINITIONS}")
-	set_target_properties(${TARGET} PROPERTIES INCLUDE_DIRECTORIES "${ARDUINO_INCLUDES}")
-	set_target_properties(${TARGET} PROPERTIES LINK_FLAGS "-Os -Wl,--gc-sections,--defsym=__rtc_localtime=0 --specs=nano.specs -mcpu=${CPU} -mthumb -T${LINKER_FILE}")
-	set_target_properties(${TARGET} PROPERTIES OUTPUT_NAME ${ELFTARGET})
+    set_target_properties(${TARGET} PROPERTIES COMPILE_DEFINITIONS "${TEENSY_DEFINITIONS}")
+    set_target_properties(${TARGET} PROPERTIES INCLUDE_DIRECTORIES "${ARDUINO_INCLUDES}")
+    set_target_properties(${TARGET} PROPERTIES LINK_FLAGS "-Os -Wl,--gc-sections,--defsym=__rtc_localtime=0 --specs=nano.specs -mcpu=${CPU} -mthumb -T\"${LINKER_FILE}\"")
+    set_target_properties(${TARGET} PROPERTIES OUTPUT_NAME ${ELFTARGET})
 
-	add_custom_command(OUTPUT ${TARGET}.hex
-	                   COMMAND ${COMPILERPATH}arm-none-eabi-size ${ELFTARGET}
-	                   COMMAND ${COMPILERPATH}arm-none-eabi-objcopy -O ihex -R .eeprom ${ELFTARGET} ${TARGET}.hex
-	                   COMMAND ${TOOLSPATH}teensy_post_compile -file=${TARGET} -path=${CMAKE_CURRENT_BINARY_DIR} -tools=${TOOLSPATH}
-	                   DEPENDS ${TARGET}
-	                   COMMENT "Creating HEX file for ${TARGET}")
+    add_custom_command(OUTPUT ${TARGET}.hex
+                       COMMAND ${COMPILERPATH}arm-none-eabi-size ${ELFTARGET}
+                       COMMAND ${COMPILERPATH}arm-none-eabi-objcopy -O ihex -R .eeprom ${ELFTARGET} ${TARGET}.hex
+                       COMMAND ${TOOLSPATH}teensy_post_compile -file=${TARGET} -path=${CMAKE_CURRENT_BINARY_DIR} -tools=${TOOLSPATH}
+                       DEPENDS ${TARGET}
+                       COMMENT "Creating HEX file for ${TARGET}")
 
-	add_custom_target(${TARGET}-hex ALL DEPENDS ${TARGET}.hex)
+    add_custom_target(${TARGET}-hex ALL DEPENDS ${TARGET}.hex)
 endfunction()
 
 function(teensy_add_library TARGET)
 	add_library(${TARGET} ${ARGN})
 
     set_target_properties(${TARGET} PROPERTIES COMPILE_FLAGS "-Wall -g -Os -mcpu=${CPU} -mthumb -nostdlib -MMD -felide-constructors -fno-exceptions -fno-rtti -std=gnu++11")
-	set_target_properties(${TARGET} PROPERTIES COMPILE_DEFINITIONS "${TEENSY_DEFINITIONS}")
-	set_target_properties(${TARGET} PROPERTIES INCLUDE_DIRECTORIES "${ARDUINO_INCLUDES}")
+    set_target_properties(${TARGET} PROPERTIES COMPILE_DEFINITIONS "${TEENSY_DEFINITIONS}")
+    set_target_properties(${TARGET} PROPERTIES INCLUDE_DIRECTORIES "${ARDUINO_INCLUDES}")
 endfunction()
