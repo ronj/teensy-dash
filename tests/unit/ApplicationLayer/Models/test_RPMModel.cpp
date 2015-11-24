c6d58368d856578cb5d29a9af57b52c99cfa114e
Ron Jaegers <ron.jaegers@gmail.com>
2015-11-24 18:57:58 +0100

    Improved test coverage a bit.

--- tests/unit/ApplicationLayer/Models/test_RPMModel.cpp
+++ tests/unit/ApplicationLayer/Models/test_RPMModel.cpp
@@ -18,3 +18,22 @@ TEST(RPMModelTest, should_have_correct_initial_value)
 	EXPECT_EQ(0, rpm.GetRawValue());
 	EXPECT_EQ("0", rpm.GetFormattedValue());
 }
+
+TEST(RPMModelTest, should_return_correct_rpm_based_on_measured_frequency)
+{
+	std::list<std::tuple<float, int32_t, std::string>> expected = { std::make_tuple(50.f, 2000, "2000"),
+		                                                            std::make_tuple(60.f, 2400, "2400"),
+			                                                        std::make_tuple(63.55f, 2542, "2500") };
+
+	ApplicationLayer::Models::RPMModel rpm(*frequencyCounter);
+
+	for (auto&& e : expected)
+	{
+		mocks.ExpectCall(frequencyCounter, PeripheralLayer::FrequencyCounter::GetFrequency).Return(std::get<0>(e));
+
+		rpm.Update(0);
+
+		EXPECT_EQ(std::get<1>(e), rpm.GetRawValue());
+		EXPECT_EQ(std::get<2>(e), rpm.GetFormattedValue());
+	}
+}
--- tests/unit/PeripheralLayer/test_Color.cpp
+++ tests/unit/PeripheralLayer/test_Color.cpp
@@ -20,6 +20,13 @@ struct ColorTest
 		EXPECT_EQ(v, hsv.v);
 	}
 
+	void check_hsl(uint8_t h, uint8_t s, uint8_t l, const Color::HSL& hsl)
+	{
+		EXPECT_EQ(h, hsl.h);
+		EXPECT_EQ(s, hsl.s);
+		EXPECT_EQ(l, hsl.l);
+	}
+
 	void hsv_rgb_hsv_conversion_helper(uint8_t h, uint8_t s, uint8_t v)
 	{
 		Color::HSV expected(h, s, v);
@@ -29,6 +36,17 @@ struct ColorTest
 		EXPECT_EQ(expected.h, actual.h);
 		EXPECT_EQ(expected.s, actual.s);
 		EXPECT_EQ(expected.v, actual.v);
+	}
+
+	void hsl_rgb_hsl_conversion_helper(uint8_t h, uint8_t s, uint8_t l)
+	{
+		Color::HSL expected(h, s, l);
+		Color::RGB rgb = expected;
+		Color::HSL actual = rgb;
+
+		EXPECT_EQ(expected.h, actual.h);
+		EXPECT_EQ(expected.s, actual.s);
+		EXPECT_EQ(expected.l, actual.l);
 	}
 };
 
@@ -45,9 +63,20 @@ TEST(ColorTest, should_construct_hsv_color)
 	check_hsv(30, 100, 100, Color::HSV(30, 100, 100));
 }
 
-TEST(ColorTest, should_convert_colorspaces)
+TEST(ColorTest, should_construct_hsl_color)
 {
+	check_hsl(0, 0, 0, Color::HSL());
+	check_hsl(80, 90, 60, Color::HSL(80, 90, 60));
+}
+
+TEST(ColorTest, should_convert_rgb_hsv_colorspaces)
+{
+	hsv_rgb_hsv_conversion_helper(0, 0, 0);
 	hsv_rgb_hsv_conversion_helper(20, 255, 100);
-	hsv_rgb_hsv_conversion_helper(0, 0, 150);
+	hsv_rgb_hsv_conversion_helper(0, 0, 255);
+	//hsv_rgb_hsv_conversion_helper(0, 255, 0);
+	//hsv_rgb_hsv_conversion_helper(255, 0, 255);
 	//hsv_rgb_hsv_conversion_helper(11, 22, 33);
+
+	//hsl_rgb_hsl_conversion_helper(10, 20, 30);
 }
